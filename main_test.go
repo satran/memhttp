6103b0aae8b59eaf773c9a27541029a26a8b8b8f
Satyajit Ranjeev <s@ranjeev.in>
2021-12-15 21:47:26 +0100

    Add simple test

--- main_test.go
+++ main_test.go
@@ -3,5 +3,25 @@ package main
 import "testing"
 
 func TestWalk(t *testing.T) {
-	walk("/home/satran/src/blog", ".git")
+	files, err := walk(".", ".git")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, exp := range []string{
+		"go.mod",
+		"main.go",
+		"main_test.go",
+		"readme.md",
+	} {
+		found := false
+		for name := range files {
+			if name == exp {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("expected: %s", exp)
+		}
+	}
 }
